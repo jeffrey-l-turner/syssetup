aad9063e262336e92960d9680649db48dc2c1c7e
Jeff Turner <Jeffrey.L.Turner@me.com>
2014-04-15 09:42:50 -0400

    updated to include vim configuration and emacs/vim selection

--- setup.sh
+++ setup.sh
@@ -7,6 +7,11 @@ nvmuse="v0.10.19"
 # location of dotfiles on Git
 gitdotfiles="https://github.com/jeffrey-l-turner/dotfiles.git"
 
+# location of vundle on Git
+vundle="https://github.com/gmarik/vundlegit"
+vimColorSchemes="git@github.com:dgvigil/vim-colorschemes.git"
+    
+
 lowercase(){
     echo "$1" | sed "y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/"
 }
@@ -39,7 +44,7 @@ shootProfile(){
     if [ "${OS}" == "darwin" ]; then
         OS="mac"
         REV=`uname -r`
-        PSUEDONAME="Darwin"
+        PSEUDONAME="Darwin"
         DistroBasedOn='BSD'
         AppInstall="brew "
         llopts=" "
@@ -49,24 +54,24 @@ shootProfile(){
             if [ -f /etc/redhat-release ] ; then
                 DistroBasedOn='RedHat'
                 DIST=`cat /etc/redhat-release |sed s/\ release.*//`
-                PSUEDONAME=`cat /etc/redhat-release | sed s/.*\(// | sed s/\)//`
+                PSEUDONAME=`cat /etc/redhat-release | sed s/.*\(// | sed s/\)//`
                 REV=`cat /etc/redhat-release | sed s/.*release\ // | sed s/\ .*//`
                 AppInstall="sudo yum "
             elif [ -f /etc/SuSE-release ] ; then
                 DistroBasedOn='SuSe'
-                PSUEDONAME=`cat /etc/SuSE-release | tr "\n" ' '| sed s/VERSION.*//`
+                PSEUDONAME=`cat /etc/SuSE-release | tr "\n" ' '| sed s/VERSION.*//`
                 REV=`cat /etc/SuSE-release | tr "\n" ' ' | sed s/.*=\ //`
                 AppInstall="exit 2"
             elif [ -f /etc/mandrake-release ] ; then
                 DistroBasedOn='Mandrake'
-                PSUEDONAME=`cat /etc/mandrake-release | sed s/.*\(// | sed s/\)//`
+                PSEUDONAME=`cat /etc/mandrake-release | sed s/.*\(// | sed s/\)//`
                 REV=`cat /etc/mandrake-release | sed s/.*release\ // | sed s/\ .*//`
                 AppInstall="exit 2"
             elif [ -f /etc/debian_version ] ; then
                 DistroBasedOn='Debian'
                 if [ -f /etc/lsb-release ] ; then
                         DIST=`cat /etc/lsb-release | grep '^DISTRIB_ID' | awk -F=  '{ print $2 }'`
-                            PSUEDONAME=`cat /etc/lsb-release | grep '^DISTRIB_CODENAME' | awk -F=  '{ print $2 }'`
+                            PSEUDONAME=`cat /etc/lsb-release | grep '^DISTRIB_CODENAME' | awk -F=  '{ print $2 }'`
                             REV=`cat /etc/lsb-release | grep '^DISTRIB_RELEASE' | awk -F=  '{ print $2 }'`
                         fi
                 AppInstall="sudo apt-get "
@@ -79,7 +84,7 @@ shootProfile(){
             readonly OS
             readonly DIST
             readonly DistroBasedOn
-            readonly PSUEDONAME
+            readonly PSEUDONAME
             readonly REV
             readonly KERNEL
             readonly MACH
@@ -124,8 +129,22 @@ genGitHub(){
     echo "Note: you must still upload your key to your GitHub profile!"
 }
 
-exportFlags(){
-export "githubKey" "herokuKey"
+setFlags(){
+    readonly "githubKey"
+    readonly  "herokuKey"
+    readonly "editorInstall" 
+}
+
+# These are functions to setup to toggle vim and emacs installation
+# vim will be installed along with vundle and the refactor colorscheme (via .vimrc) by default
+editorInstall=vim
+toggleVimEmacs(){
+        if [ "${editorInstall}" == "vim" ] ; then
+            editorInstall="emacs"
+        else
+            editorInstall="vim"
+        fi  
+
 }
 
 shootProfile
@@ -136,17 +155,23 @@ shootProfile
 printMenu(){
     
     clear
-    echo -e '\n\033[47;35m'"\033[1mHeadless Server setup for Node.js, rlwrap, Heroku toolbelt, and standard config files                           "
-    echo -e "See: $gitdotfiles for the list of configuration files that will be installed\033[0m\n"
+    echo -e '\n\033[47;35m'"\033[1m    Headless Server setup for Node.js, rlwrap, Heroku toolbelt, and standard config files as well as                 "
+    echo -e "         setting up a standard emacs or vim developer environment depending upon specified configuration below.      "
+    echo -e "See: $gitdotfiles for the repository with the configuration files to be installed\033[0m\n"
     echo "OS: $OS"
     echo "DIST: $DIST"
-    echo "PSUEDONAME: $PSUEDONAME"
+    echo "PSEUDONAME: $PSEUDONAME"
     echo "REV: $REV"
     echo "DistroBasedOn: $DistroBasedOn"
     echo "KERNEL: $KERNEL"
     echo "MACH: $MACH"
     echo "Will use node version: $nvmuse" 
     echo "Application Installer: $AppInstall"  
+    if [ "${editorInstall}" = "vim" ] ; then
+         echo "Editor and configuration to be installed: "$editorInstall  
+    else
+         echo "Editor and configuration to be installed: "$editorInstall  
+    fi
     if [ "${OS}" == "mac" ]; then
         echo -e '\n\033[43;35m'"Mac OS users should note that this installation script relies on the use of Homebrew and may conflict"
         echo -e "with use of Macports or Fink!\033[0m\n"
@@ -156,17 +181,25 @@ printMenu(){
     echo "=============================================================================================================="
     echo -e "\t1) Generate Heroku Key (~/.ssh/heroku-rsa) and install Heroku Toolbelt"
     echo -e "\t2) Generate GitHub Key (~/.ssh/github-rsa)"
-    echo -e "\t3) Continue Setup"
+    if [ "${editorInstall}" = "vim" ] ; then
+        echo -e "\t3) Toggle to install emacs instead of vim "
+    else
+        echo -e "\t3) Toggle to install vim instead of emacs "
+    fi
     echo -e "\t4) Exit Now!"
+    echo -e "\t5) Continue Setup"
     echo "Press ^C, q or 4 if the above system information is not correct or you wish to abort installation"
-    echo "Press press 3 to proceed"
+    echo " "
+    echo "Press press 5 to proceed"
     read option;
-    while [[ $option -gt 12 || ! $(echo $option | grep '^[1-4qQ]$') ]]
+    while [[ $option -gt 12 || ! $(echo $option | grep '^[1-5qQ]$') ]]
     do
         printMenu
     done
-    if [ "$option" == "3" ]; then
+    if [ "$option" == "5" ]; then
         echo "Starting installation..."
+        echo 
+        wait 1
         return;
     fi
     runOption
@@ -179,10 +212,11 @@ runOption(){
     case $option in
         1) genHeroku;;
         2) genGitHub;;
-        3) exportFlags;;
+        3) toggleVimEmacs;;
         4) exit;;
         q) exit;;
-        Q) exit
+        Q) exit;;
+        5) setFlags
     esac 
     echo "Press return to continue"
     read x
@@ -238,17 +272,32 @@ else
     $AppInstall install mongodb-10gen
 fi
 
+# Select whether to install vim or emacs configuration/files:
+if [ "${editorInstall}" == "emacs" ] ; then
+
 # Install emacs24 on other OSes than Mac OS
-if [ "${OS}" == "mac" ]; then
-    $AppInstall install --cocoa emacs 
-elif [ "${DistroBasedOn}" == "redhat" ]; then
-    echo "Must manually install emacs24 on RHEL"
-    echo "      Emacs not installed!!"
-else
+    if [ "${OS}" == "mac" ]; then
+        $AppInstall install --cocoa emacs 
+    elif [ "${DistroBasedOn}" == "redhat" ]; then
+        echo "Must manually install emacs24 on RHEL"
+        echo "      Emacs not installed!!"
+    else
 # https://launchpad.net/~cassou/+archive/emacs
-    $AppInstall add-apt-repository -y ppa:cassou/emacs
-    $AppInstall update
-    $AppInstall install -y emacs24 emacs24-el emacs24-common-non-dfsg
+        $AppInstall add-apt-repository -y ppa:cassou/emacs
+        $AppInstall update
+        $AppInstall install -y emacs24 emacs24-el emacs24-common-non-dfsg
+    fi
+else
+# Install VIM configuration files including vundle and colorschemes
+    if [ -d $HOME/.vim/ ]; then
+        mkdir $HOME/.vim/bundle
+    else
+        mkdir $HOME/.vim
+        mkdir $HOME/.vim/bundle
+    fi
+    # These use configuration specified in dotfiles/.vimrc:
+    git clone $vundle $HOME/vim/bundle/vundle
+    git clone $vimColorSchemes $HOME/.vim
 fi
 
 # Call to put dotfiles in place
@@ -267,8 +316,16 @@ ln $lnopts dotfiles/.bashrc .
 # append to custom rc file rather than linking -- this is changed from Balaji's script
 cat dotfiles/.bashrc_custom >> .bashrc_custom
 ln $lnopts dotfiles/.bash_logout .
-ln $lnopts dotfiles/.vimrc
-ln -sf dotfiles/.emacs.d .
+
+# Select whether to link vim or emacs dotfiles:
+if [ "${editorInstall}" == "emacs" ] ; then
+    ln -sf dotfiles/.emacs.d .
+else
+    ln $lnopts dotfiles/.vimrc
+    # Install bundles and plugins for vim
+    vim +PluginInstall +qall
+    vim +BundleInstall +qall
+fi
 
 #If using Mac, copy terminal settings files over to home as well
 if [ "${OS}" == "mac" ]; then
