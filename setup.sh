aa741ae4f831090c92c506f22b4e884bbaba89a3
Jeff <jeffrey.l.turner@me.com>
2014-09-25 12:05:34 -0700

    updated to correct install command for homebrew

--- setup.sh
+++ setup.sh
@@ -201,7 +201,7 @@ shootProfile
 
 # If using Mac OS, then install brew & ctags
 if [ "${OS}" == "mac" ]; then
-    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     brew install ctags
 fi
 
