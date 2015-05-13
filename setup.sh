8280d74d40330f422707a750600de4b907d8627f
Jeff Turner <JeffTu@fox.com>
2015-05-12 17:22:02 -0700

    completed port to CentOS

--- setup.sh
+++ setup.sh
@@ -155,14 +155,15 @@ installBashCompletion (){
             $AppInstall install bash-completion
             # source bash completion file:
             echo '# setup bash completion' >> $HOME/.bashrc_custom
-            echo 'if [ -f `brew --prefix`/etc/bash_completion ]; then' >> $HOME/.bashrc_custom
-            echo '       . `brew --prefix`/etc/bash_completion' >> $HOME/.bashrc_custom
-            echo 'fi' >> $HOME/.bashrc_custom
         #else if [ "${OS}" == "cygwin" ]; then
             # Download and place git-flow-completion.bash in %CYGWIN_INSTALLATION_DIR%\etc\bash_completion.d
             # Rename it to git-flow
             #echo "bash completion not installaed on Cygwin"
-        else
+        #else if [ "${OS}" == "mac" ]; then
+            echo 'if [ -f `brew --prefix`/etc/bash_completion ]; then' >> $HOME/.bashrc_custom
+            echo '       . `brew --prefix`/etc/bash_completion' >> $HOME/.bashrc_custom
+            echo 'fi' >> $HOME/.bashrc_custom
+        else 
             $AppInstall install bash-completion
         fi
     fi
@@ -228,11 +229,17 @@ shootProfile(){
     else
         OS=`uname`
         if [ "${OS}" = "Linux" ]; then
-            if [ -f /etc/redhat-release ]; then
-                DistroBasedOn='RedHat'
+            if [ -f /etc/centos-release ]; then
+                DistroBasedOn='redhat'
+                DIST=`cat /etc/centos-release | sed 's/ *Linux.*//I'`
+                PSEUDONAME=`cat /etc/centos-release | sed s/.*\(// | sed s/\)//`
+                REV=`cat /etc/centos-release | sed s/.*release\ // | sed s/\ .*//`
+                AppInstall="sudo yum "
+            elif [ -f /etc/redhat-release ]; then
+                DistroBasedOn='redhat'
                 DIST=`cat /etc/redhat-release |sed s/\ release.*//`
                 PSEUDONAME=`cat /etc/redhat-release | sed s/.*\(// | sed s/\)//`
-                REV=`cat /etc/redhat-release | sed s/.*release\ // | sed s/\ .*//`
+                REV=`cat /etc/centos-release | sed s/.*release\ // | sed s/\ .*//`
                 AppInstall="sudo yum "
             elif [ -f /etc/SuSE-release ]; then
                 DistroBasedOn='SuSe'
@@ -546,7 +553,19 @@ set -u # exit if undefined variables
 
   # Install rlwrap to provide libreadline features with node
   # See: http://nodejs.org/api/repl.html#repl_repl
-  $AppInstall install -y rlwrap
+  if [ "${DIST}" == "CentOS" ] ; then # CentOS requires compilation from source with dependencies
+    $AppInstall install readline-devel 
+    curl http://git.savannah.gnu.org/cgit/readline.git/snapshot/readline-master.tar.gz > /tmp/readline-master.tar.gz
+    pushd /tmp/
+    tar -zxvf /tmp/readline-master.tar.gz
+    cd readline-master
+    ./configure
+    make
+    sudo make install
+    popd
+  else
+    $AppInstall install -y rlwrap
+  fi 
 else # install node globally via binary
   npm="npm"
   if [ nodeInstalled == "false" ] ; then
@@ -568,7 +587,7 @@ fi
 #Install MongoDB; see: http://docs.mongodb.org/manual/tutorial/install-mongodb-on-ubuntu/
 if [ "${OS}" == "mac" ]; then
   $AppInstall install mongodb 
-elif [ "${DistroBasedOn}" == "redhat" ]; then
+elif [ "${DistroBasedOn}" == "redhat" ]; then 
     echo "Must manually install MongoDB on RHEL"
     echo "       Mongo DB not installed!!"
 elif [ "${OS}" == "cygwin" ] ; then
@@ -601,7 +620,7 @@ elif [ "${OS}" != "cygwin" ] ; then
 # Install VIM configuration files including vundle and colorschemes
     # These use configuration specified in dotfiles/.vimrc:
     if [ -d $HOME/.vim/bundle ]; then
-        mv $HOME/.vim/bundle $HOME/.vim/bundle.old
+        rm -rf $HOME/.vim/bundle 
     fi
     mkdir -p $HOME/.vim/bundle
     git clone $vundle $HOME/.vim/bundle/vundle
@@ -625,6 +644,12 @@ elif [ "${editorInstall}" == "vim" ] ; then
     if [ "${OS}" != "cygwin" ] ; then 
         rm -f "${HOME}/.vimrc"
         cp -f "${HOME}/dotfiles/.vimrc" "${HOME}"
+
+        # setup vim on CentOS
+        if [ "${DIST}" == "CentOS" ] ; then
+            $AppInstall install  vim-X11 vim-common vim-enhanced vim-minimal
+            echo "alias vim vi " >> ~/.bashrc_custom
+        fi 
 
     # setup pathogen specific installs by using git clones
         cd "${HOME}/.vim" 
