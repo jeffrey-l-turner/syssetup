772ae6eb04609ce602c351fcb706a8a90ddfe3a0
Jeff Turner <Jeffrey.L.Turner@me.com>
2016-05-03 17:25:25 -0700

    changing to LTS node version

--- setup.sh
+++ setup.sh
@@ -57,7 +57,7 @@ fi
 
 #datetime=$(date +%Y%m%d%H%M%S)
 # Version of Node to use:
-nvmuse="v0.4.0" 
+nvmuse="4.4.3" 
 # binary of node to use on Windows/Cygwin
 winNode="http://nodejs.org/dist/${nvmuse}/x64/node-${nvmuse}-x64.msi"
 
